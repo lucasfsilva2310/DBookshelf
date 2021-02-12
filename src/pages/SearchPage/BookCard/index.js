4a58552b3c6b9a9f72ad2ad4f2898dea58ef9abb
Lucas Ferreira <lucasfsilva2310gmail.com>
2021-02-12 15:04:24 -0300

    Added a map method to the BookInfo component

--- src/pages/SearchPage/BookCard/index.js
+++ src/pages/SearchPage/BookCard/index.js
@@ -3,98 +3,54 @@ import { BookCard, ImageBookContainer, InfoBookContainer } from "./styled";
 
 const BookInfo = ({ Books }) => {
   // Books sao os dados recebidos pelo state global
+  //adicionar state global
 
   return (
     <BookCardContainer>
-      {/* Inserir Map de livros aqui */}
-      {/* Card Exemplo:
-        <BookCard>
-        <ImageBookContainer>
-          <img
-            src="https://i.postimg.cc/0j8WH9Z8/pngegg-4.png"
-            alt="book cape"
-          />
-        </ImageBookContainer>
-        <InfoBookContainer>
-          <h1>Title</h1>
-          <h2>Author: author</h2>
-          <p>Publisher: Publisher</p>
-          <p>Published: d-a-t-e</p>
-        </InfoBookContainer>
-      </BookCard>
-      */}
-      <BookCard>
-        <ImageBookContainer>
-          <img
-            src="https://i.postimg.cc/0j8WH9Z8/pngegg-4.png"
-            alt="book cape"
-          />
-        </ImageBookContainer>
-        <InfoBookContainer>
-          <h1>Title</h1>
-          <h2>Author: author</h2>
-          <p>Publisher: Publisher</p>
-          <p>Published: d-a-t-e</p>
-        </InfoBookContainer>
-      </BookCard>
-      <BookCard>
-        <ImageBookContainer>
-          <img
-            src="https://i.postimg.cc/0j8WH9Z8/pngegg-4.png"
-            alt="book cape"
-          />
-        </ImageBookContainer>
-        <InfoBookContainer>
-          <h1>Title</h1>
-          <h2>Author: author</h2>
-          <p>Publisher: Publisher</p>
-          <p>Published: d-a-t-e</p>
-        </InfoBookContainer>
-      </BookCard>
-      <BookCard>
-        <ImageBookContainer>
-          <img
-            src="https://i.postimg.cc/0j8WH9Z8/pngegg-4.png"
-            alt="book cape"
-          />
-        </ImageBookContainer>
-        <InfoBookContainer>
-          <h1>Title</h1>
-          <h2>Author: author</h2>
-          <p>Publisher: Publisher</p>
-          <p>Published: d-a-t-e</p>
-        </InfoBookContainer>
-      </BookCard>
-      <BookCard>
-        <ImageBookContainer>
-          <img
-            src="https://i.postimg.cc/0j8WH9Z8/pngegg-4.png"
-            alt="book cape"
-          />
-        </ImageBookContainer>
-        <InfoBookContainer>
-          <h1>Title</h1>
-          <h2>Author: author</h2>
-          <p>Publisher: Publisher</p>
-          <p>Published: d-a-t-e</p>
-        </InfoBookContainer>
-      </BookCard>
-      <BookCard>
-        <ImageBookContainer>
-          <img
-            src="https://i.postimg.cc/0j8WH9Z8/pngegg-4.png"
-            alt="book cape"
-          />
-        </ImageBookContainer>
-        <InfoBookContainer>
-          <h1>Title</h1>
-          <h2>Author: author</h2>
-          <p>Publisher: Publisher</p>
-          <p>Published: d-a-t-e</p>
-        </InfoBookContainer>
-      </BookCard>
+      {BookTest.map((book, index) => {
+        return (
+          <BookCard>
+            <ImageBookContainer>
+              <img src={book.img} alt="book cape" />
+            </ImageBookContainer>
+            <InfoBookContainer>
+              <h1>{book.title}</h1>
+              <h2>{`Author: ${book.author}`}</h2>
+              <p>{`Publisher: ${book.publisher}`}</p>
+              <p>{`Published: ${book.published}`}</p>
+            </InfoBookContainer>
+          </BookCard>
+        );
+      })}
     </BookCardContainer>
   );
 };
 
 export default BookInfo;
+
+const BookTest = [
+  {
+    title: "Harry Potter - A Coleção Completa",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61p1DfZTTAL._AC_SL1000_.jpg",
+    author: "David Yates",
+    publisher: "Daniel Radcliffe",
+    published: "10 julho 2020",
+  },
+  {
+    title: "Harry Potter - A Coleção Completa",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61p1DfZTTAL._AC_SL1000_.jpg",
+    author: "David Yates",
+    publisher: "Daniel Radcliffe",
+    published: "10 julho 2020",
+  },
+  {
+    title: "Harry Potter - A Coleção Completa",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61p1DfZTTAL._AC_SL1000_.jpg",
+    author: "David Yates",
+    publisher: "Daniel Radcliffe",
+    published: "10 julho 2020",
+  },
+];
